f47883e16b8a14e38225603ed2c762399e404857
shashank.agrawal <shashank.agrawal@metricstream.com>
2023-04-18 10:00:17 +0530

    doing changes to learn MVC

--- Gym project backend/src/main/java/com/telusko/springmvcboot/security/SecurityConfig.java
+++ Gym project backend/src/main/java/com/telusko/springmvcboot/security/SecurityConfig.java
@@ -1,65 +1,73 @@
-package com.telusko.springmvcboot.security;
-
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
-
-@Configuration
-
-public class SecurityConfig {
-
-    @Bean
-    public UserDetailsManager userDetailsManager(DataSource dataSource){
-        return new JdbcUserDetailsManager(dataSource);
-    }
-
+//package com.telusko.springmvcboot.security;
+//
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.provisioning.JdbcUserDetailsManager;
+//import org.springframework.security.provisioning.UserDetailsManager;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//import javax.sql.DataSource;
+//
+//@Configuration
+//
+//public class SecurityConfig {
+//
 //    @Bean
-//    public InMemoryUserDetailsManager userDetailsManagr() {
-//        UserDetails shashank = User.builder().username("shashank").password("{noop}shashank").roles("admin").build();
-//        UserDetails pranay = User.builder().username("pranay").password("{noop}shashank").roles("user").build();
-//        UserDetails manjeet = User.builder().username("manjeet").password("{noop}shashank").roles("contributfffor")
-//                .build();
+//    public UserDetailsManager userDetailsManager(DataSource dataSource){
+//       JdbcUserDetailsManager jdbcUserDetailsManager= new JdbcUserDetailsManager(dataSource);
 //
-//        return new InMemoryUserDetailsManager(shashank, pranay, manjeet);
+//       //this will fetch user deyails from members table. it is created using data.sql file
+//       jdbcUserDetailsManager.setUsersByUsernameQuery("select user_id,pw,active from members where user_id=?");
+//       //this will fetch details from roles for authorization
+//       jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id,role from roles where user_id=?");
 //
+//
+//        return jdbcUserDetailsManager;
 //    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests(configure -> configure
-                .requestMatchers(HttpMethod.GET,"/test").hasRole("MANAGER"));
-        http.httpBasic();
-        http.csrf().disable();
-
-        return http.build();
-    }
-
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests((authorizeHttpRequests) ->
-//                        authorizeHttpRequests
-//                                .antMatchers("/addnewplan").hasAnyRole("user")
-//                ).formLogin();
 //
+////    @Bean
+////    public InMemoryUserDetailsManager userDetailsManagr() {
+////        UserDetails shashank = User.builder().username("shashank").password("{noop}shashank").roles("admin").build();
+////        UserDetails pranay = User.builder().username("pranay").password("{noop}shashank").roles("user").build();
+////        UserDetails manjeet = User.builder().username("manjeet").password("{noop}shashank").roles("contributfffor")
+////                .build();
+////
+////        return new InMemoryUserDetailsManager(shashank, pranay, manjeet);
+////
+////    }
 //
-//        //this is for HTTP Basic Authentication, so that user can send username nd passwrd along with rest request
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+//        http.authorizeHttpRequests(configure -> configure
+//                .requestMatchers(HttpMethod.GET,"/test").hasRole("MANAGER"));
 //        http.httpBasic();
+//        http.csrf().disable();
 //
-//        http       //other configure params.
-//                .csrf().disable();
-//
+//        return http.build();
 //    }
-
-}
+//
+////    @Override
+////    protected void configure(HttpSecurity http) throws Exception {
+////        http
+////                .authorizeHttpRequests((authorizeHttpRequests) ->
+////                        authorizeHttpRequests
+////                                .antMatchers("/addnewplan").hasAnyRole("user")
+////                ).formLogin();
+////
+////
+////        //this is for HTTP Basic Authentication, so that user can send username nd passwrd along with rest request
+////        http.httpBasic();
+////
+////        http       //other configure params.
+////                .csrf().disable();
+////
+////    }
+//
+//}
--- Gym project backend/src/main/resources/data.sql
+++ Gym project backend/src/main/resources/data.sql
@@ -3,32 +3,53 @@ INSERT INTO gymplan (body_part,exercise1,exercise2,exercise3,exercise4,exercise5
 INSERT INTO gymplan (body_part,exercise1,exercise2,exercise3,exercise4,exercise5) VALUES ('Leg',' SQUAT$23kg','calves$5kg','calfs$2.5kg','ham$10kg','RDL$5kg');
 
 
-DROP TABLE IF EXISTS `authorities`;
-DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `roles`;
+DROP TABLE IF EXISTS `members`;
 
-CREATE TABLE `users` (
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `enabled` tinyint NOT NULL,
-  PRIMARY KEY (`username`)
+--
+-- Table structure for table `members`
+--
+
+CREATE TABLE `members` (
+  `user_id` varchar(50) NOT NULL,
+  `pw` char(68) NOT NULL,
+  `active` tinyint NOT NULL,
+  PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO users (username,password,enabled) VALUES ('john','{noop}test123',1);
-INSERT INTO users (username,password,enabled) VALUES ('mary','{noop}test123',1);
-INSERT INTO users (username,password,enabled) VALUES ('susan','{noop}test123',1);
+--
+-- Inserting data for table `members`
+--
+-- NOTE: The passwords are encrypted using BCrypt
+--
+-- A generation tool is avail at: https://www.luv2code.com/generate-bcrypt-password
+--
+-- Default passwords here are: fun123
+--
+
+INSERT INTO `members`
+VALUES
+('john','{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',1),
+('mary','{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',1),
+('susan','{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',1);
 
 
+--
+-- Table structure for table `authorities`
+--
 
-
-CREATE TABLE `authorities` (
-  `username` varchar(50) NOT NULL,
-  `authority` varchar(50) NOT NULL,
-  UNIQUE KEY `authorities_idx_1` (`username`,`authority`),
-  CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+CREATE TABLE `roles` (
+  `user_id` varchar(50) NOT NULL,
+  `role` varchar(50) NOT NULL,
+  UNIQUE KEY `authorities5_idx_1` (`user_id`,`role`),
+  CONSTRAINT `authorities5_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `members` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Inserting data for table `roles`
+--
 
-INSERT INTO `authorities`
+INSERT INTO `roles`
 VALUES
 ('john','ROLE_EMPLOYEE'),
 ('mary','ROLE_EMPLOYEE'),
